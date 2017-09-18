faddb42c4727f57f8aa98ac9780e501f7d07f832
Vitor <vitorbigelli@hotmail.com>
2017-09-18 19:20:39 -0300

    Saving progress.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-$(function() {
+var app = app || {};
 
-	var Food = Backbone.Model.extend({
+(function($) { 
+	'use strict';
+
+	//------------- MODEL -------------//
+
+	app.Food = Backbone.Model.extend({
 		defaults: {
 			name: "No food",
 			calories: 0 
@@ -8,93 +13,108 @@ $(function() {
 
 	}); 
 
+	//------------- COLLECTIONS -------------//
+
 	var FoodList = Backbone.Collection.extend({
+		model: app.Food,
 
-		model: Food,
-
-		getFoods: function() {
-			return this;
-		}
-
+		localStorage: new Backbone.LocalStorage("foods-backbone")
 	});
 
-	var selectedFoods = new FoodList([]);
-
-	var foods = new FoodList([]);
-
+	app.selectedFoods = new app.FoodList();
+	app.search = new app.FoodList();
 
 
+	//------------- VIEW -------------//
 
-	var App = Backbone.View.extend({
+	app.FoodView = Backbone.View.extend({
+
+		tagName: 'li', 
+
+		events: {
+			"click .add": "addToSelectedFoods"			
+		},
 
 		initialize: function() {
 
-			this.listenTo(foods, "add", this.render);
+			this.$searchResults = document.getElementById("search-result-list");
+			var foodName, foodCalories;
+			this.listenTo(app.search, "add", this.addOne);
 
-			function searchFood() {
-
-				var search = document.getElementById("search-field").value;
-				search = "orange";
-
-				var url = "https://api.nutritionix.com/v1_1/search/"+ search +
-						"?results=0%3A20&cal_min=0&cal_max=50000&" + 
-						"fields=*";
-
-				url += "&" + $.param({
-					"appId": "d0c20d56",
-					"appKey": "719fa1c3a70de3b074a9463c4db07eb5"
-				});
-
-				$.getJSON(url, function(data) {
-					var items = data.hits;
-					var currentFoods =[];
-					var length = items.length;
-					console.log(url);
-					document.getElementById("search-result-list").innerHTML = "";
-
-
-					for (var i=0; i < length; i++) {
-						console.log("Adding: " +  items[i].fields.item_name);
-						food = new Food({
-							name: items[i].fields.item_name,
-							calories: items[i].fields.nf_calories
-						});
-						foods.push(food);
-					}
-				})
-				.fail( function(error) {
-					window.alert("Error trying to access Nutriotionix")
-				})
-			};
-
-			document.getElementById("submit-button").addEventListener("click", searchFood);
 		},
 
 		render: function() {
-			var $searchResults = document.getElementById("search-result-list");
-			var foodName, foodCalories;
-
-			foodName = food.attributes.name;
-			foodCalories = food.attributes.calories;
-			$searchResults.innerHTML += "<li>" + 
-										"<p class='food'>" + foodName + "</p>" +
-										"<span class='info'>" + foodCalories + " calories</span>" + 
-										"<span class='add'><i class='fa fa-plus' aria-hidden='true'></i></span>" + 
-										"<span class='add-result'></span></li>";
-
-			this.item = $("li:last-of-type"); 
-			this.add = this.item.children(".add")[0];
-			
-			this.add.addEventListener("click", (function() {
-				console.log("Clicked");
-			}), this );
-
+			foodId = food.id,
+			foodName = food.name;
+			foodCalories = food.calories;
+			app.AppView.searchFood.$searchResults.innerHTML += "<li id=" + foodId + ">" + 
+										"<p class='food'><span>" + foodName + "</span>" +
+										"<br><span class='info'>" + foodCalories + " calories</span></p>" + 
 			return this;
-		}
+		}, 
 
+		addOne: function(food) {
+			console.log(food);
+		},
+
+		addToSelectedFoods: function(foodId) {
+			console.log("Selected");
+		}
+	});
+
+	app.AppView = Backbone.View.extend({
+
+		el: "header",
+
+		events: {
+			"click #submit-search": "searchFood",
+			"click #stats": "showSelectedFoods",
+		},
+
+		initialize: function() {
+			this.listenTo(this.foods, "add", this.addOne)
+			this.$searchResults = document.getElementById("#search-results-list");
+			
+		},
+
+		showSelectedFoods: function(event) {
+			$(document.getElementsByTagName("aside")[0]).toggleClass("slide");
+		}, 
+
+		searchFood: function() {
+			var self = this;
+			this.$searchResults = $(document.getElementById("search-field")).val();
+			
+			var url = "https://api.nutritionix.com/v1_1/search/"+ this.$search +
+					"?results=0%3A20&cal_min=0&cal_max=50000&" + 
+					"fields=*";
+
+			url += "&" + $.param({
+				"appId": "d0c20d56",
+				"appKey": "719fa1c3a70de3b074a9463c4db07eb5"
+			});
+
+			$.getJSON(url, function(data) {
+				var items = data.hits;
+				var currentFoods =[];
+				var length = items.length;
+
+				for (var i=0; i < length; i++) {
+					console.log("Adding: " +  items[i].fields.item_name);
+					food = {
+						id: items[i].fields.item_id,
+						name: items[i].fields.item_name,
+						calories: items[i].fields.nf_calories
+					};
+					app.searchResult.push(food);
+				}
+			})
+			.fail( function(error) {
+				window.alert("Error trying to access Nutriotionix")
+			})
+		}
 
 
 	});
 
-	new App();
-});
+})(jQuery);
